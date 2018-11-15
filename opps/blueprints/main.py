d30658b42d9cdc9d451e559477c8ecba3e66454d
Pandalis <shacowork@gmail.com>
2018-11-15 17:41:04 +0800

    Update main.py

--- opps/blueprints/main.py
+++ opps/blueprints/main.py
@@ -17,8 +17,11 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return render_template('main/index.html')
-    #return redirect(url_for('auth.login'))
+    if current_user.is_authenticated:
+        return render_template('main/index.html')
+    else:
+        return redirect(url_for('auth.login'))
+
 
 
 
